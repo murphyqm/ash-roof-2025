fd0362961951e965e740ea9e2bf8a4d38c21de87
Maeve Murphy Quinlan <mmurphyquinlan@gmail.com>
2025-07-03 11:16:48 +0000

    Create pseudocode

---
+++ src/pythag/tri.py
@@ -0,0 +1,14 @@
+# Module to calculate hypot of triangle
+
+# hypot calc function
+# takes lengths a and b (opposite and adjacent)
+# of a triangle
+# and returns c, the hypotenuse
+
+# squaring function
+# takes length a
+# returns a^2
+
+# adding function
+# takes lengths a and b
+# returns a + b
